5b95b51ab0c0b1ce7bf4daa5c34b82c166d4bd1d
Adil GTC <ezarfi.adil.it@gmail.com>
2026-02-24 16:56:45 +0000

    new

--- Order.API/obj/Release/net8.0/Order.API.AssemblyInfo.cs
+++ Order.API/obj/Release/net8.0/Order.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Order.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acded321e22b20e49949ca4b3b826bb69f10c7e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73195a3f39fc4663a11cf85e270bda28675de040")]
 [assembly: System.Reflection.AssemblyProductAttribute("Order.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Order.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
