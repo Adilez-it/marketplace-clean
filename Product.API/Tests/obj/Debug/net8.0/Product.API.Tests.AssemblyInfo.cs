969e94a4aff1c25716ad8b44423f9e135ae3ebb1
Adil GTC <ezarfi.adil.it@gmail.com>
2026-02-24 13:40:17 +0000

    changes

--- Product.API/Tests/obj/Debug/net8.0/Product.API.Tests.AssemblyInfo.cs
+++ Product.API/Tests/obj/Debug/net8.0/Product.API.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Product.API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc022dcc5e0af7cfe7377296d3bb92b1c530695")]
 [assembly: System.Reflection.AssemblyProductAttribute("Product.API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Product.API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
